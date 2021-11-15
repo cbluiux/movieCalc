6c92c659a043813519430f631d5be01692afda19
cbluiux <cbluiux@gmail.com>
2021-11-14 23:58:14 -0700

    introduced error and loading state

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,6 +6,8 @@ import SearchBar from '../components/SearchBar'
 const HomeScreen = () => {
   const [searchResult, setSearchResult] = useState({})
   const [movies, setMovies] = useState([])
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState('')
 
   const paramFormatter = (term) => {
     return term.split().join('+')
@@ -14,13 +16,28 @@ const HomeScreen = () => {
   const searchApi = async (term) => {
     setSearchResult({})
 
+    if (error) {
+      setError('')
+    }
+
+    setLoading('Loading...')
+
     try {
       const response = await axios.get(
         `http://www.omdbapi.com/?apikey=4464198b&t=${paramFormatter(term)}`
       )
-      console.log(response.data, 'THIS IS RESPONSE.DATA')
+
+      if (response.data.Error) {
+        setLoading('')
+        setError(response.data.Error)
+        return
+      }
+
+      setLoading('')
       setSearchResult(response.data)
     } catch (e) {
+      setLoading('')
+      setError('Sorry, something went wrong! Please try your search again.')
       console.error(e)
     }
   }
@@ -29,6 +46,8 @@ const HomeScreen = () => {
     <ScrollView style={{ padding: 100 }}>
       <Text>This is my Home Screen</Text>
       <SearchBar onTermSubmit={searchApi} />
+      {loading ? <Text>{loading}</Text> : null}
+      {error ? <Text>{error}</Text> : null}
     </ScrollView>
   )
 }
