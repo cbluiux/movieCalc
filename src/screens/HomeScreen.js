886f3a3031fc230aa626622ae6b337492c7df993
cbluiux <cbluiux@gmail.com>
2021-11-14 23:14:36 -0700

    Created HomeScreen

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ScrollView, Text, StyleSheet } from 'react-native'
+
+const HomeScreen = () => {
+  return (
+    <ScrollView>
+      <Text>This is my Home Screen</Text>
+    </ScrollView>
+  )
+}
